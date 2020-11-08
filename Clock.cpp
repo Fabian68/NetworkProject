2eba6bb0730f6252f084815bc9398215376efe85
QuentinB <48256560+Yeperllow@users.noreply.github.com>
2020-11-08 23:58:39 +0100

    Modified Clock class
    FIX - Corrected time and toString function not returning a correct value when the clock had started or stopped respectively
    ADD - Can now add a time at the initialization
    ADD - Can chose the unit of time (one tick equals to 1 minutes, hours ...)

--- Clock.cpp
+++ Clock.cpp
@@ -1,37 +1,60 @@
 #include "Clock.h"
+#include <sstream>
 
-Clock::Clock(int hourStart) : _startedTime{}
+/**
+* Can't add minutes when considering 1 tick to be 1 hour so secondStart has to be higher than howManySecondsInOneTick
+*/
+Clock::Clock(int secondStart, int howManySecondsInOneTick) : _startedTime{}, _secondsInOneTick{ howManySecondsInOneTick }, _clockOngoing{false}
 {
-	_totalTime = chrono::seconds(0);
-	_totalTime += chrono::seconds(hourStart);
+	_totalTime = chrono::seconds(secondStart / howManySecondsInOneTick);
 }
 
 void Clock::start()
 {
 	_startedTime = chrono::steady_clock::now();
+	_clockOngoing = true;
 }
 
 string Clock::toString()
 {
-	chrono::steady_clock::time_point tempTime = chrono::steady_clock::now();
-	chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(tempTime - _startedTime);
-	chrono::duration<double> temporaryTime = _totalTime + time_span;
-	int timespan = temporaryTime.count();
-	int seconde = timespan % 86400 * 60;
-	int hour = seconde / 3600;
-	int minute = (seconde % 3600) / 60;
-	string toString = hour + "h";
-	toString += minute + "minutes";
-	return toString;
+	if (_clockOngoing == true) 
+	{
+		chrono::steady_clock::time_point tempTime = chrono::steady_clock::now();
+		chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(tempTime - _startedTime);
+		chrono::duration<double> temporaryTime = time_span + _totalTime;
+		int timespan = temporaryTime.count();
+		int seconde = timespan % 86400 * _secondsInOneTick;
+		int hour = (seconde / 3600);
+		int minute = (seconde % 3600) / 60;
+		string toString = std::to_string(hour) + " heures, " + std::to_string(minute) + " minutes, " + std::to_string(seconde) + " secondes";
+		return toString;
+	}
+	else 
+	{
+		int timespan = _totalTime.count();
+		int seconde = timespan % 86400 * _secondsInOneTick;
+		int hour = (seconde / 3600);
+		int minute = (seconde % 3600) / 60;
+		string toString = std::to_string(hour) + " heures, " + std::to_string(minute) + " minutes, " + std::to_string(seconde) + " secondes";
+		return toString;
+	}
 }
 
 double Clock::time()
 {
-	chrono::steady_clock::time_point tempTime = chrono::steady_clock::now();
-	chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(tempTime - _startedTime);
-	chrono::duration<double> temporaryTime = _totalTime + time_span;
-	int timespan = temporaryTime.count();
-	return timespan % 86400 * 60;
+	if (_clockOngoing == true)
+	{
+		chrono::steady_clock::time_point tempTime = chrono::steady_clock::now();
+		chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(tempTime - _startedTime);
+		chrono::duration<double> temporaryTime = time_span + _totalTime;
+		int timespan = temporaryTime.count();
+		return timespan % 86400 * _secondsInOneTick;
+	}
+	else
+	{
+		int timespan = _totalTime.count();
+		return timespan % 86400 * _secondsInOneTick;
+	}
 }
 
 void Clock::stop()
@@ -39,4 +62,5 @@ void Clock::stop()
 	chrono::steady_clock::time_point endedTime = chrono::steady_clock::now();
 	chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(endedTime - _startedTime);
 	_totalTime += time_span;
+	_clockOngoing = false;
 }
--- Clock.h
+++ Clock.h
@@ -8,7 +8,7 @@ using namespace std;
 class Clock
 {
 public :
-	Clock(int hourStart = 0);
+	Clock(int secondStart = 0, int howManySecondsInOneTick = 60);
 	void start();
 	string toString();
 	double time();
@@ -16,6 +16,8 @@ public :
 private :
 	chrono::duration<double> _totalTime;
 	chrono::steady_clock::time_point _startedTime;
+	int _secondsInOneTick;
+	bool _clockOngoing;
 };
 
 #endif
