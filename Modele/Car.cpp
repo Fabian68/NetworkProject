6cd954c51cac2364b165caaa1b32ec7db6768a1a
Gaetan1999 <coulleraygaetan@outlook.fr>
2021-01-06 16:09:46 +0100

    Ajout Gaëtan
    Voilà un premier ajout avec le vecteur pour les voitures avec son getter, la fonction ajouter un véhiculer s'il peut communiquer avec un autre et supprimer.

--- Modele/Car.cpp
+++ Modele/Car.cpp
@@ -1,7 +1,9 @@
 #include "Car.h"
 
 Car::Car(const Point &CarPosition, const Wave &WaveCom, const Way &way, double speed) : _PositionOfCar{CarPosition}, _WaveComunication{WaveCom}, _Way{way}, _Speed{speed}
-{}
+{
+    Car::_CommunicationVToV.reserve(100);
+}
 
 Point Car::CarPosition() const
 {
@@ -23,15 +25,31 @@ double Car::Speed() const
     return _Speed;
 }
 
+vector<Car> Car::CarCommunicate() const
+{
+    return _CommunicationVToV;
+}
+
 void Car::CarMovingOn(const Point &MovePoint)
 {
     Point OldPosition = CarPosition();
     _PositionOfCar = MovePoint;
 }
 
-void Car::CarComunicating(const Car &OtherCar)
+void Car::AddCarCommunicating(const Car &OtherCar)
 {
-    // ???
+    // Calcul si on peut communiquer avec l'autre voiture
+}
+
+void Car::DeleteCarCommunicating()
+{
+    if(!_CommunicationVToV.empty())
+    {
+        for(int i = 0 ; i < _CommunicationVToV.size() ; i++)
+        {
+            // Test si la voiture en [i] peut communiquer
+        }
+    }
 }
 
 void Car::SetCarWay(const Way &OtherWay)
