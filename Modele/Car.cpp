482260e146eda0a490bfbd4feb9a457167dd1c3a
bragoskelet <luca.campeglia@gmail.com>
2021-01-13 14:19:04 +0100

    correction erreur rand dans Car

--- Modele/Car.cpp
+++ Modele/Car.cpp
@@ -118,7 +118,7 @@ void Car::changeRoute(Node* endingNode, Way* finishedWay)
         int iWay;
         do
         {
-            int iWay = rand() * endingNode->getConnectedWays().size();
+            int iWay = rand() % endingNode->getConnectedWays().size();
         } while (endingNode->getConnectedWays()[iWay]->getId() == finishedWay->getId());
 
         setWay(endingNode->getConnectedWays()[iWay]);
